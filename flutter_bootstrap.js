3fa951bfe2663dc407cc0b82cffc41d5dd529769
rednaxcela <lixanddrr@gmail.com>
2026-08-20 13:40:45 +0800

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2053926615" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4227117821" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
